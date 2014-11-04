68771c6fdd72dec5b1f317e7c3c5e6271b91b4b8
Sergey Kholodniy <sekh@ciklum.com>
2014-11-04 16:35:07 +0200

    Test commit. Removed index.html to app/ folder

--- scripts/logic.js
+++ scripts/logic.js
@@ -4,11 +4,11 @@ var x = 1;
 
 function changeIcon(){
 	if (x % 2 == 0) {
-		document.getElementById("clickme").src = "img/icon.jpg";
+		document.getElementById("clickme").src = "../img/icon.jpg";
 		x++;
 	}
 	else{
-		document.getElementById("clickme").src = "img/inco1.jpg";
+		document.getElementById("clickme").src = "../img/inco1.jpg";
 		x++;
 	}
 
