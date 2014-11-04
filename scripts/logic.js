6168b7411d86eaee715922e321dfe7e2a38b4ca2
Sergey Kholodniy <sekh@ciklum.com>
2014-11-04 13:38:13 +0200

    Adds image to the center on index page and also adds a script that changes that picture by clicking on it. Also perform little formating. Most of the files are moved to appropriate directories.

---
+++ scripts/logic.js
@@ -0,0 +1,16 @@
+
+var x = 1;
+
+
+function changeIcon(){
+	if (x % 2 == 0) {
+		document.getElementById("clickme").src = "img/icon.jpg";
+		x++;
+	}
+	else{
+		document.getElementById("clickme").src = "img/inco1.jpg";
+		x++;
+	}
+
+	
+}
